e2cb1195e2dbb5123f5700fa6a277a1b580f70bb
jaimejuniordt <jaimejuniordt@gmail.com>
2023-05-16 22:42:19 -0300

    4jogoreact-dia2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
+/* eslint-disable no-unused-vars */
+// CSS
+import "./App.css";
+
+// React
+import { useCallback, useEffect, useState } from "react";
+
+// data
+import { wordsList } from "./data/words";
+
+// components
+import StartScreen from './components/StartScreen';
+import GameOver from './components/GameOver';
+import Game from './components/Game';
+
+// estágios do jogo
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 function App() {
+  const [gameStage, ] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  console.log(words);
+
   return (
     <div className="App">
-      <h2>Secret Word</h2>
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "game" && <Game />}
+      {gameStage === "end" && <GameOver />}
     </div>
   );
 }
